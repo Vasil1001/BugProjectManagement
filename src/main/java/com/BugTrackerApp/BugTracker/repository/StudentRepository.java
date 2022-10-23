1930a0f374135949ccd794eae7ed971b984a3072
Vasil1001 <vaskodzhakov@gmail.com>
2022-10-23 22:52:29 +0100

    Clean up

--- src/main/java/com/BugTrackerApp/BugTracker/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.BugTrackerApp.BugTracker.repository;
-
-import com.BugTrackerApp.BugTracker.model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-}
